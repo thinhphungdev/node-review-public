1d80beb7ef0ee5de7b2b16fe18dc0ca1a94865ba
thinhphungdev <phungthinh0946519356@gmail.com>
2024-02-02 16:08:06 +0700

    routing with express

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -34,35 +34,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/subdir', express.static(path.join(__dirname, 'public')));
 
-const one = function (req, res, next) {
-  console.log('one');
-  next();
-};
-
-const two = function (req, res, next) {
-  console.log('two');
-  next();
-};
-
-app.get('^/$|/index(.html)', (req, res) => {
-  //   res.send('Hello world!');
-  //   res.sendFile('./views/index.html', { root: __dirname });
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
-app.get('/new-page(.html)', (req, res) => {
-  //   res.send('Hello world!');
-  //   res.sendFile('./views/index.html', { root: __dirname });
-  res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page(.html)', (req, res) => {
-  res.redirect(301, 'new-page.html');
-});
-
-app.get('/chain(.html)?', [one, two]);
+app.use('/subdir', require('./routes/subdir'));
+app.use('/', require('./routes/root'));
+app.use('/employees', require('./api/employee'));
 
 app.all('/*', (req, res) => {
   res.status(404);
