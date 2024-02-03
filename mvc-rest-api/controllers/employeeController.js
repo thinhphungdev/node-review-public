10ed3da7f92da8491fcc7880149f86d879313192
thinhphungdev <phungthinh0946519356@gmail.com>
2024-02-03 11:56:24 +0700

    employee controller

--- mvc-rest-api/controllers/employeeController.js
+++ mvc-rest-api/controllers/employeeController.js
@@ -27,18 +27,65 @@ function createNewEmployee(req, res) {
 }
 
 function updateEmployee(req, res) {
-  res.json({
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-  });
+  const employee = data.employees.find(
+    (employee) => employee.id === parseInt(req.body.id)
+  );
+
+  const { firstname, lastname } = req.body;
+
+  if (!employee)
+    return res.status(400).json({
+      message: `Employee ID ${req.body.id} not found`,
+    });
+
+  const updatedEmployee = {
+    firstname: firstname || employee.firstname,
+    lastname: lastname || employee.lastname,
+    id: employee.id,
+  };
+
+  const removedOldEmployeeArray = data.employees.filter(
+    (employee) => employee.id !== parseInt(req.body.id)
+  );
+
+  const unsortedArray = removedOldEmployeeArray.push(updatedEmployee);
+  data.setEmployees(
+    unsortedArray.sort((a, b) => (a.id > b.id ? 1 : a.id < b.id ? -1 : 0))
+  );
+
+  res.json(data.employees);
 }
 
 function deleteEmployee(req, res) {
-  res.json({ id: req.body.id });
+  const employee = data.employees.find(
+    (employee) => employee.id === +req.body.id
+  );
+
+  if (!employee)
+    return res.status(400).json({
+      message: `Employee ID ${req.body.id} does not exist`,
+    });
+
+  const filteredArray = data.employees.filter(
+    (employee) => employee.id !== req.body.id
+  );
+
+  data.setEmployees([...filteredArray]);
+
+  res.json(data.employees);
 }
 
 function getEmployee(req, res) {
-  res.json({ id: req.params.id });
+  const employee = data.employees.find(
+    (employee) => employee.id === +req.body.id
+  );
+
+  if (!employee)
+    return res.status(400).json({
+      message: `Employee ID ${req.body.id} does not exist`,
+    });
+
+  res.json(employee);
 }
 
 module.exports = {
